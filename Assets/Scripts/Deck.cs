b843d6b032786749c9cd916fbe58285c4d9d451d
IllicLanthresh <miqueldreamer@gmail.com>
2017-05-24 23:56:43 +0200

    A lot of stuff srry no specific commits

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour {
+
+   public enum side {Runner, Corp};
+
+    //This stores which side the deck is(Runner or Corp)
+    public side DeckSide;
+
+    List<CardDBEntry> ListOfCards = new List<CardDBEntry>();
+
+
+    public List<CardDBEntry> parseDeckFile(TextAsset deckFile, CardDBEntry[] cardDBEntries)
+    {
+
+        foreach (string line in deckFile.text.Split(System.Environment.NewLine.ToCharArray(), System.StringSplitOptions.None))
+        {
+            
+            if (line != null && line != "")
+            {
+                int nOfCards;
+                string nameOfCard;
+                char[] Xletter = { 'x' };
+
+                nOfCards = int.Parse(line.Split(Xletter)[0]);
+                nameOfCard = line.Split(Xletter, 2)[1].Trim();
+
+                for (int i = 0; i < nOfCards; i++)
+                {
+                    Debug.Log("Looking for " + nameOfCard + " on DB");
+                    foreach (CardDBEntry cardDBEntry in cardDBEntries)
+                    {
+
+                        if (cardDBEntry.title == nameOfCard)
+                        {
+                            Debug.Log("Found: " + cardDBEntry.title + ", adding...");
+                            ListOfCards.Add(cardDBEntry);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return ListOfCards;
+    }
+
+    public IEnumerator startDeck(GameObject cardPrefab)
+    {
+       //Multiplier to put each card on top of the other
+        int increment = 0; //TODO: Find a better way to do this
+        string previousCardCode = null;
+        Texture2D imgCarta = new Texture2D(0,0);
+        
+        foreach (CardDBEntry card in ListOfCards)
+        {
+            if (previousCardCode != card.code)
+            {
+                string img_url = CardDB.GetBaseImgUrl().Replace("{code}", card.code);
+                WWW url = new WWW(img_url);
+                Debug.Log("Downloading image for " + card.title);
+
+                while (!url.isDone)
+                {
+                    //Debug.Log("Downloading image: " + url.progress * 100 + "%");
+                    yield return null;
+                }
+                imgCarta = url.texture;
+                Debug.Log("Done!");                
+            }
+            else
+            {
+                Debug.Log("Using the same image.");
+            }
+            previousCardCode = card.code;
+
+            Vector3 YincrementVector = new Vector3(0, 0.01f);
+            Vector3 FlipCardRotation = new Vector3(0, 0, 180f);
+            
+            //Create the card GO
+            GameObject cardGO = Instantiate(cardPrefab, this.transform.position + YincrementVector*increment, this.transform.rotation).gameObject;
+            cardGO.transform.Rotate(FlipCardRotation, Space.World);
+
+            cardGO.transform.SetParent(this.transform);
+
+            cardGO.name = card.title;
+
+            //Use the image downloaded
+            cardGO.transform.Find("Card_model").Find("front").GetComponent<Renderer>().material.mainTexture = imgCarta;
+
+            //Put the correct back image of the card depending on which side the card is(Runner or Corp)
+            if (card.side_code == "runner")
+            {
+                cardGO.transform.Find("Card_model").Find("back").GetComponent<Renderer>().material.mainTexture = Resources.Load<Texture2D>("Textures/Runner_back");
+            }
+            else if (card.side_code == "corp")
+            {
+                cardGO.transform.Find("Card_model").Find("back").GetComponent<Renderer>().material.mainTexture = Resources.Load<Texture2D>("Textures/Corp_back");
+            }
+            increment++;
+        }
+        Debug.Log(DeckSide.ToString() + " side finished downloading card images");
+
+    }
+
+    public void Shuffle()
+    { 
+        for (int i = 0; i < ListOfCards.Count; i++)
+        {
+            CardDBEntry temp = ListOfCards[i];
+            int randomIndex = Random.Range(i, ListOfCards.Count);
+            ListOfCards[i] = ListOfCards[randomIndex];
+            ListOfCards[randomIndex] = temp;
+        }
+        UpdateVisual();
+    }
+
+    public void UpdateVisual()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Game : MonoBehaviour
+{
+
+    static string api_url = "https://netrunnerdb.com/api/2.0/public/cards";
+
+    // References to the basic prefabs(filled in ispector)
+    public GameObject deckPrefab;
+    public GameObject cardPrefab;
+
+    //Paths to deck lists, also filled in inspector(Currently using a TXT file)
+    //The format is '(Quantity)x(Name of card)' -- Ignore the '()'
+    public TextAsset RunnerDeckTxt;
+    public TextAsset CorpDeckTxt;
+    
+    //Callback after the api data has been downloaded
+    Action<string> apiDownloadedCB;
+    CardDBEntry[] cardDBEntries;
+
+    GameObject RunnerAreaGO;
+    GameObject CorpAreaGO;
+
+    GameObject RunnerDeckGO;
+    GameObject CorpDeckGO;
+
+    Deck RunnerDeck;
+    Deck CorpDeck;
+
+
+    void Start () {
+        
+        //Register the 'api downloaded' callback function
+        apiDownloadedCB += OnAPIDownloaded;
+
+        //Download the api data
+        CardDB.SetApiUrl(api_url);
+        StartCoroutine(CardDB.DownloadAPI(apiDownloadedCB));
+
+        //Get the game areas GO
+        RunnerAreaGO = GameObject.FindGameObjectWithTag("Runner");
+        CorpAreaGO = GameObject.FindGameObjectWithTag("Corp");
+
+        setupDecks();
+    }
+
+    void OnAPIDownloaded(string parsedApi)
+    {
+        cardDBEntries =  CardDB.ParseApi(parsedApi);
+
+        OnApiParsed();
+    }
+
+    void setupDecks()
+    {
+        //Create decks GO
+        RunnerDeckGO = Instantiate(deckPrefab, Vector3.zero, Quaternion.identity).gameObject;
+        CorpDeckGO = Instantiate(deckPrefab, Vector3.zero, Quaternion.identity).gameObject;
+
+        //Get the deck managers
+        RunnerDeck = RunnerDeckGO.GetComponent<Deck>();
+        CorpDeck = CorpDeckGO.GetComponent<Deck>();
+
+        //Set deck names for hierarchy
+        RunnerDeckGO.name = "Runner Deck";
+        CorpDeckGO.name = "Corp Deck";
+
+        //Create Vectors for deck placement(position and rotation)
+        Vector3 CorpDeckPosition = new Vector3(3.5f, 0.25f, -5f);
+        Vector3 CorpDeckRotation = new Vector3(90, 0, 0);
+        Vector3 RunnerDeckPosition = new Vector3(-3.5f, 0.25f, 5f);
+        Vector3 RunnerDeckRotation = new Vector3(90, 180, 0);
+
+        //Apply such positions and rotations
+        //TODO: Not final positions, this will change!
+        CorpDeckGO.transform.Translate(CorpDeckPosition);
+        RunnerDeckGO.transform.Translate(RunnerDeckPosition);
+        CorpDeckGO.transform.Rotate(CorpDeckRotation, Space.World);
+        RunnerDeckGO.transform.Rotate(RunnerDeckRotation, Space.World);
+
+        //Set decks as a child of its player's game area
+        CorpDeckGO.transform.SetParent(CorpAreaGO.transform, true);
+        RunnerDeckGO.transform.SetParent(RunnerAreaGO.transform, true);
+
+        //Tell each Deck Manager wich side is it(Runner or Corp)
+        CorpDeck.DeckSide = Deck.side.Corp;
+        RunnerDeck.DeckSide = Deck.side.Runner;
+    }
+
+    void OnApiParsed()
+    {
+        //Parse deck files
+        List<CardDBEntry> CorpCardsList = CorpDeck.parseDeckFile(CorpDeckTxt, cardDBEntries);
+        List<CardDBEntry> RunnerCardsList = RunnerDeck.parseDeckFile(RunnerDeckTxt, cardDBEntries);
+
+        //Get the cards for the decks
+        StartCoroutine(CorpDeck.startDeck(cardPrefab));
+        StartCoroutine(RunnerDeck.startDeck(cardPrefab));
+        
+
+        //TODO: Get identity card out(of the deck)
+
+        //TODO: Shuffle decks
+
+        //TODO: Start turn manager
+
+        //TODO: More stuff???
+    }
+}
